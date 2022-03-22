1976d33ee36b16eead82a983594a537b725e5ea0
drewseffi <drewsutherland23@gmail.com>
2022-03-22 00:23:12 +0000

    Added new activity to test

---
+++ app/src/main/java/com/example/testyourself/WorkingScreen.java
@@ -0,0 +1,14 @@
+package com.example.testyourself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_working_screen);
+    }
+}
